61733ce43cd1650196c9f1f2f2ef415a6ef8d832
Beygee <doug0476@naver.com>
2020-05-29 14:37:25 +0900

    하단 Sheet 애니메이션화

--- lib/exhibition_bottom_sheet.dart
+++ lib/exhibition_bottom_sheet.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 const double minHeight = 120;
+const double iconStartSize = 44;
+const double iconEndSize = 120;
+const double iconStartMarginTop = 36;
+const double iconEndMarginTop = 80;
+const double iconsVerticalSpacing = 24;
+const double iconsHorizontalSpacing = 16;
 
 class ExhibitionBottomSheet extends StatefulWidget {
   @override
@@ -14,6 +20,21 @@ class _ExhibitionBottomSheetState extends State<ExhibitionBottomSheet>
   AnimationController _controller;
 
   double get maxHeight => MediaQuery.of(context).size.height;
+
+  double get headerTopMargin =>
+      lerp(20, 20 + MediaQuery.of(context).padding.top);
+  double get headerFontSize => lerp(14, 24);
+
+  double get itemBorderRadius => lerp(8, 24);
+  double get iconSize => lerp(iconStartSize, iconEndSize);
+
+  double iconTopMargin(int index) =>
+      lerp(iconStartMarginTop,
+          iconEndMarginTop + index * (iconsVerticalSpacing + iconEndSize)) +
+      headerTopMargin;
+
+  double iconLeftMargin(int index) =>
+      lerp(index * (iconsHorizontalSpacing + iconStartSize), 0);
 
   @override
   void initState() {
@@ -55,10 +76,42 @@ class _ExhibitionBottomSheetState extends State<ExhibitionBottomSheet>
                   top: Radius.circular(32),
                 ),
               ),
+              child: Stack(
+                children: <Widget>[
+                  MenuButton(),
+                  SheetHeader(
+                    fontSize: headerFontSize,
+                    topMargin: headerTopMargin,
+                  ),
+                  for (Event event in events) _buildIcon(event),
+                ],
+              ),
             ),
           ),
         );
       },
+    );
+  }
+
+  Widget _buildIcon(Event event) {
+    int index = events.indexOf(event);
+
+    return Positioned(
+      height: iconSize,
+      width: iconSize,
+      top: iconTopMargin(index),
+      left: iconLeftMargin(index),
+      child: ClipRRect(
+        borderRadius: BorderRadius.horizontal(
+          left: Radius.circular(itemBorderRadius),
+          right: Radius.circular(itemBorderRadius),
+        ),
+        child: Image.asset(
+          'assets/${event.assetName}',
+          fit: BoxFit.cover,
+          alignment: Alignment(lerp(1, 0), 0),
+        ),
+      ),
     );
   }
 
@@ -86,4 +139,70 @@ class _ExhibitionBottomSheetState extends State<ExhibitionBottomSheet>
     final bool isOpen = _controller.status == AnimationStatus.completed;
     _controller.fling(velocity: isOpen ? -2 : 2);
   }
+}
+
+class MenuButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0,
+      bottom: 24,
+      child: Icon(Icons.menu, color: Colors.white, size: 28),
+    );
+  }
+}
+
+class SheetHeader extends StatelessWidget {
+  final double fontSize;
+  final double topMargin;
+
+  SheetHeader({
+    Key key,
+    @required this.fontSize,
+    @required this.topMargin,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: topMargin,
+      child: Text(
+        'Booked Exhibition',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: fontSize,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+}
+
+final List<Event> events = [
+  Event('steve-johnson.jpeg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('efe-kurnaz.jpg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('rodion-kutsaev.jpeg', 'Dawan District Guangdong Hong Kong', '4.28-31'),
+  Event('steve-johnson.jpeg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('efe-kurnaz.jpg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('rodion-kutsaev.jpeg', 'Dawan District Guangdong Hong Kong', '4.28-31'),
+  Event('steve-johnson.jpeg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('efe-kurnaz.jpg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('rodion-kutsaev.jpeg', 'Dawan District Guangdong Hong Kong', '4.28-31'),
+  Event('steve-johnson.jpeg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('efe-kurnaz.jpg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('rodion-kutsaev.jpeg', 'Dawan District Guangdong Hong Kong', '4.28-31'),
+  Event('steve-johnson.jpeg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('efe-kurnaz.jpg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('rodion-kutsaev.jpeg', 'Dawan District Guangdong Hong Kong', '4.28-31'),
+  Event('steve-johnson.jpeg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('efe-kurnaz.jpg', 'Shenzhen GLOBAL DESIGN AWARD 2018', '4.20-30'),
+  Event('rodion-kutsaev.jpeg', 'Dawan District Guangdong Hong Kong', '4.28-31'),
+];
+
+class Event {
+  final String assetName;
+  final String title;
+  final String date;
+
+  Event(this.assetName, this.title, this.date);
 }
